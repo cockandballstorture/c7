0c7dbe3c3dc28278dba69e91592db83af92b62a7
cockandballstorture <72618474+cockandballstorture@users.noreply.github.com>
2020-12-18 15:08:47 +0300

    Create ex4.c

---
+++ ex4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void main()
+{
+    int x,y,i;
+    char *p;
+    char h[255];
+    char q[5]="STOP";
+    x=0;
+    y=0;
+    scanf("%s",h);
+    if (strcmp(h,q)!=0){
+      p = (char*) malloc(strlen(h) * sizeof(char));
+      *p=h;
+      x=1;
+      y=strlen(h);
+      for(i=0;i<strlen(h);i++){
+            h[i]=0;
+        }
+      
+    }
+    else{
+        exit(0);
+        x=0;
+        y=0;
+    }
+    while(1==1){
+        scanf("%s",h);
+        if (strcmp(h,q)!=0){
+        x++;
+        p = (char*) realloc(p,strlen(h) * sizeof(char));
+        *p=h;
+        y=y+strlen(h);
+        }
+        else{
+        printf("%d,%d",x,y);
+        free(p);
+        exit(0);
+        }
+        for(i=0;i<strlen(h);i++){
+            h[i]=0;
+        }
+    }
+    y=y/x;
+    printf("%d,%d",x,y);
+    free(p);
+}
